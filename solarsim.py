53ab76acae05bee3ae88afe5e202baad21d0525b
Wesley Reuver <reuverwesley@gmail.com>
2019-11-24 12:28:13 +0100

    Add initial project files

---
+++ solarsim.py
@@ -0,0 +1,13 @@
+class Planet:    
+    def __init__(self, mass, xpos, ypos, velocity, ):
+        self.mass = mass
+        self.xpos = xpos
+        self.ypos = ypos
+        self.velocity = velocity
+
+    def update(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
